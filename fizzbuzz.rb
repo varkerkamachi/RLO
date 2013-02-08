cdea2f56ef83750db0faa19a6855b766d733ee30
Victor Goff <keeperotphones@gmail.com>
2013-02-07 23:56:47 -0600

    fizzbuzz refactored

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,22 +1,30 @@
-def fizz_buzz( r1, r2 )
-  (r1..r2).each do |n|
-    puts calc_fizz_or_buzz( n )
-  end
+# doctest: fizz buzz returns integers occording to rules
+# >> fizzbuzz -15
+# => 'FizzBuzz'
+# >> fizzbuzz 0
+# => 'FizzBuzz'
+# >> fizzbuzz 1
+# => 1
+# >> fizzbuzz 2
+# => 2
+# >> fizzbuzz 3
+# => 'Fizz'
+# >> fizzbuzz 5
+# => 'Buzz'
+#
+def fizzbuzz( n )
+  results = ''
+   results << "Fizz" if n % 3 == 0
+   results << "Buzz" if n % 5 == 0
+   results.empty? ? n : results
 end
-def calc_fizz_or_buzz( n )
-  if n % 3 == 0 && n % 5 == 0
-    "FizzBuzz"
-  elsif n % 5 == 0
-    "Buzz"  
-  elsif n % 3 == 0
-    "Fizz"
-  else
-    n
-  end
-end
-# puts calc_fizz_or_buzz(50)
-#fizz_buzz(1, 100)
 
--15.upto(15) do |number|
-  puts calc_fizz_or_buzz(number)
-end
+begin
+  # puts calc_fizz_or_buzz(50)
+  # fizz_buzz(1, 100)
+
+  results = -15.upto(15).map do |number|
+    fizzbuzz number
+  end
+  puts results.inspect
+end if __FILE__ == $0
