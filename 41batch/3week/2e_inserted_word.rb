9e2bd9b2e2da716bec267a80c788b31d3eecb370
Andrew Cranston <andrew.cranston@gree.co.jp>
2013-04-22 21:36:19 -0700

    exercise 2, inserted word

---
+++ 41batch/3week/2e_inserted_word.rb
@@ -0,0 +1,29 @@
+def get_file_contents( f )
+  File.read( f )
+end
+def replace_contents( text, str, replacement )
+  text.to_s.gsub!(str, replacement)
+end
+def write_contents_to_file( f, text )
+  File.open(f, 'w') do |n|
+    n.puts text
+  end
+end
+def replace_file_contents( f, str, replacement )
+  c = get_file_contents( f ).split('\n').each {|line| replace_contents(line, str, replacement)}
+  write_contents_to_file( f, c )
+end
+replace_file_contents( 'word.txt', 'word', 'inserted word' )
+
+
+def recreate_initial_file( file, og_file )
+  return 'file already exists' if Dir.entries(Dir.getwd).include?(file)
+  f = File.new(file, 'w+')
+  c = get_file_contents( og_file ).split('\n').each {|line|
+    f.write(line )
+  }
+  f.close
+end
+
+
+puts recreate_initial_file( 'reset.txt', 'word.txt' )
