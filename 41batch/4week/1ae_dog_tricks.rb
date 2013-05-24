c90e3b88611a4d0682885e66e4bf5dcc5a7add71
Andrew Cranston <andrewtcranston@gmail.com>
2013-05-23 22:27:50 -0700

    adding challenge exercise for dog

--- 41batch/4week/1ae_dog_tricks.rb
+++ 41batch/4week/1ae_dog_tricks.rb
@@ -31,25 +31,39 @@
 # and will tell a certain dog to perform a trick it hasn't learned yet...
 
 class Dog
-  attr_accessor :name, :poo
+  attr_accessor :name
   def initialize( name = 'mutt' )
     @name = name
-    @trickstate = []
   end
   
-  def teach_trick &trick
-    #passing in a proc..?
-    trick.call
+  def method_missing(m, *args, &block)
+    puts "#{@name} has not learned #{m} yet"
   end
   
-  poo = lambda { |n| puts "#{n} is a smelly doggy!" }
-  # :laugh = lambda { "#{@name} finds this hilarious!" }
-  #private @trickstate
+  def teach_trick(trick_name, &block)
+    self.class.send(:define_method, trick_name, &block)
+  end
+  
+  def bark
+    "#{@name} goes woof woof"
+  end
+  
+  def eat
+    "#{@name} says nom nom"
+  end
+  
+  def chase_cat
+    "#{@name} chases the cat"
+  end
+  
 end
 
 d = Dog.new('ralphie')
 # puts d.inspect
 puts d.name
+puts d.laugh
+d.teach_trick(:roll) { "#{@name} loves to roll around" }
+puts d.roll
 #puts d.dance
 # puts d.dance
 #d.teach_trick( :poo )
